54ef458170270a37c092805740e25149c8eeeb03
Alexander Mills <alex@oresoftware.com>
2018-09-18 10:47:15 -0700

    ores/auto-commit => 'set'

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,65 @@
 'use strict';
 
-export const foo = 'bar';
+import * as stream from 'stream';
 
 export const r2gSmokeTest = function () {
   // r2g command line app uses this exported function
   return true;
 };
 
-/*
+type EVCb<T> = (err: any, T: any) => void;
 
- bad library design:
- module.exports = 'foo';
- export = 'foo';
 
- good library design:
- exports.x = 'foo'
- export const x = 'foo'
- export default = 'foo';
+export class JSONParser extends stream.Transform {
 
-*/
+  lastLineData = '';
+  objectMode = true;
 
+  constructor() {
+    super();
+
+  }
+
+  transform(chunk: any, encoding: string, cb: Function) {
+
+    let data = String(chunk);
+    if (this.lastLineData) {
+      data = this.lastLineData + data;
+    }
+
+    let lines = data.split('\n');
+    this.lastLineData = lines.splice(lines.length - 1, 1)[0];
+
+    lines.forEach(l => {
+      try {
+        // l might be an empty string; ignore if so
+        l && this.push(JSON.parse(l));
+      }
+      catch (err) {
+        // noop
+      }
+    });
+
+    cb();
+
+  }
+
+  flush(cb: Function) {
+    if (this.lastLineData) {
+      try {
+        this.push(JSON.parse(this.lastLineData));
+      }
+      catch (err) {
+        // noop
+      }
+    }
+    this.lastLineData = '';
+    cb();
+  }
+
+
+}
+
+export default JSONParser;
 
 
